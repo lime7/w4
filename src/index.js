818585605a5870621d675191acfa1ebf31423c08
lime7 <senseless.v.7@gmail.com>
2020-10-10 19:58:10 +0300

    add tiny-slider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@
 import './sass/style.scss'
 import './js/main.js'
 
-// eslint-disable-next-line no-unused-vars
-import Alert from 'bootstrap/js/dist/alert'
-
 if (process.env.NODE_ENV === 'development') {
   require('./index.html')
-  require('./alert.html')
   require('./blog.html')
   require('./contact.html')
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 'use strict'
 
 console.log('S>>>')
+
+// Bootstrap alert
+// eslint-disable-next-line import/first
+import Alert from 'bootstrap/js/dist/alert'
+
+// Tiny Slider
+// eslint-disable-next-line import/first
+import { tns } from 'tiny-slider/src/tiny-slider'
+
+var slider = tns({
+  container: '.my-slider',
+  items: 3,
+  slideBy: 'page',
+  autoplay: true
+})
